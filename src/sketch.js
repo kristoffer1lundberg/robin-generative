3da365696eae4874a6adbc81165cd6d3ac7102c8
Kristoffer Lundberg <klundb@gmail.com>
2025-12-03 09:00:32 +0100

    ...

--- src/sketch.js
+++ src/sketch.js
@@ -1,13 +1,16 @@
 function setup() {
-    let canvas = createCanvas(800, 600);
-    canvas.parent('sketch-container');
-    background(220);
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("sketch-container");
+  background(220);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    // Your p5.js code here
-    fill(100, 150, 255);
-    noStroke();
-    ellipse(mouseX, mouseY, 50, 50);
+  // Your p5.js code here
+  fill(100, 150, 255);
+  noStroke();
+  ellipse(mouseX, mouseY, 50, 50);
 }
-
